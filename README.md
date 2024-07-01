c5b0945cedbb14e51138282fb2f269407ec2307c
Jonathan Garcia <garciajonathan268@gmail.com>
2024-06-30 19:04:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hola_mundo_tv2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/hola_mundo_tv2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hola_mundo_tv2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
